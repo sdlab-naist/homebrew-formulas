8f2c035f91240496b63e49d89ffb280ea33c8999
Kevin Saito <yum02.skullcandy@gmail.com>
2015-04-28 17:59:30 +0900

    update

--- git-logger.rb
+++ git-logger.rb
@@ -6,13 +6,14 @@ require 'formula'
 class GitLogger < ScriptFileFormula
   homepage 'https://github.com/sdlab-naist/git-logger'
   url 'https://github.com/sdlab-naist/git-logger/tarball/0.0.1'
-  sha1 '4e95cb4ba5c01c72bc2b79c9d10babdad4ec07b0'
+  sha1 '011636c9a78866f0a74a00dc138bb23f603b70ae'
   version '0.0.1'
 
   depends_on 'findutils'
 
   def install
-    bin.install 'import.sh' => 'git-logger'
+    bin.install 'bin/git-logger' => 'git-logger'
+    etc.install Dir["etc/git-logger-hooks"]
   end
 
 end
